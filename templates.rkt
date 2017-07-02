04ffe70c12c9df3e9ebf79274804485d8d675acf
Leif Andersen <leif@leifandersen.net>
2017-07-01 20:50:37 -0400

    Typo

--- templates.rkt
+++ templates.rkt
@@ -14,10 +14,10 @@
      @meta[charset: "utf-8"]
      @meta[http-equiv: "X-UA-Compatible" content: "IE=edge"]
      @meta[name: "viewport" 'content: "width=device-width, initial-scale=1"]
-     @link[rel: "stylesheet" type: "text/css" href: "css/bootstrap.min.css"]
+     @link[rel: "stylesheet" type: "text/css" href: "/css/bootstrap.min.css"]
      @link[rel: "stylesheet" type: "text/css" href: "/css/pygments.css"]
      @link[rel: "stylesheet" type: "text/css" href: "/css/scribble.css"]
-     @link[rel: "stylesheet" type: "text/css" href: "css/custom.css"]
+     @link[rel: "stylesheet" type: "text/css" href: "/css/custom.css"]
      @title[v]{ - Video Language}
      @rest})
 
