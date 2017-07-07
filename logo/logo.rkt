a96985631d8264daedf2a6f90fb68632a176368e
Leif Andersen <leif@leifandersen.net>
2017-07-07 16:26:56 -0400

    ~a -> ~s

--- logo/logo.rkt
+++ logo/logo.rkt
@@ -50,7 +50,7 @@
                              "Linux Libertine Display 0"))
       (define intersect (set-intersect (get-face-list) attempts))
       (when (set-empty? intersect)
-        (error 'mk-logo "No fount ~a installed" attempts))
+        (error 'mk-logo "No fount ~s installed" attempts))
       (car intersect)))
   (define camera-body
     (dc (λ (dc dx dy)
