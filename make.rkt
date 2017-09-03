52f0a34a46cf53e3af031c0a619a19b4be8e7e4c
Leif Andersen <leif@leifandersen.net>
2017-09-03 11:07:24 +0100

    Update build script

--- make.rkt
+++ make.rkt
@@ -1,7 +1,8 @@
 #!/usr/bin/env racket
 #lang racket
 
-(require compiler/find-exe
+(require racket/cmdline
+         compiler/find-exe
          racket/runtime-path
          scribble/html/resource
          "files.rkt")
@@ -12,7 +13,8 @@
 
 (define video-src "https://github.com/videolang/video.git")
 
-(define (build)
+(define (build #:render-resources? [rr #t]
+               #:render-blog? [rb #t])
   ;; Render HTML files
   (for ([f (in-list files)])
     (unless (equal? (path-get-extension f) #".html")
@@ -21,12 +23,14 @@
         (lambda ()
           (dynamic-require f 0)))))
   ;; Render image resources
-  (parameterize ([current-directory project-root-dir])
-    (render-all))
+  (when rr
+    (parameterize ([current-directory project-root-dir])
+      (render-all)))
   ;; Render blog
-  (parameterize ([current-directory (build-path project-root-dir "blog")])
-    (system* (find-exe) "-l" "raco" "pkg" "install" "--auto" "--skip-installed" "frog")
-    (system* (find-exe) "-l" "raco" "frog" "-b"))
+  (when rb
+    (parameterize ([current-directory (build-path project-root-dir "blog")])
+      (system* (find-exe) "-l" "raco" "pkg" "install" "--auto" "--skip-installed" "frog")
+      (system* (find-exe) "-l" "raco" "frog" "-b")))
   ;; Render current manual
   ;(define tmp-dir (make-temporary-file "rkttmp~a" 'directory))
   #;(parameterize ([current-directory tmp-dir])
@@ -38,4 +42,31 @@
                "+m" "--redirect-main" "http://docs.racket-lang.org/" "video.scrbl")
       (copy-directory/files "video" (build-path project-root-dir "manl")))))
 
-(build)
+(define render-resources? #t)
+(define render-blog? #t)
+(define preview? #f)
+
+(void
+ (command-line
+  #:program "Video Website Builder"
+  #:once-each
+  [("--disable-resources") "Do not compile resources for the website"
+                           (set! render-resources? #f)]
+  [("--disable-blog") "Do notcompile the blog"
+                      (set! render-blog? #f)]
+  [("-p" "--enable-preview" "--preview") "Preview the generated webpage"
+                                         (set! preview? #t)]))
+
+(build #:render-resources? render-resources?
+       #:render-blog? render-blog?)
+
+(when preview?
+  (define serve/servlet (dynamic-require 'web-server/servlet-env 'serve/servlet))
+  (define next-dispatcher (dynamic-require 'web-server/dispatchers/dispatch 'next-dispatcher))
+  (serve/servlet (lambda (_) (next-dispatcher))
+                 #:servlet-path "/index.html"
+                 #:extra-files-paths (list project-root-dir)
+                 #:port (integer-bytes->integer #"VD" #f)
+                 #:listen-ip #f
+                 #:launch-browser? #t))
+  
